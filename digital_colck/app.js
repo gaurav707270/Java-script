a046537c34d2705d1c167ab16b7c327ce180f56e
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-04-11 15:07:50 +0530

    Add files via upload

--- digital_colck/app.js
+++ digital_colck/app.js
@@ -1,9 +1,33 @@
-const clock = document.querySelector("#clock");
-const time = document.querySelector("#time");
+const container = document.getElementById("container")
+const day = document.getElementById("day")
+const hour = document.getElementById("hour")
+const minutes = document.getElementById("minutes")
+const second = document.getElementById("second")
+const dt = document.getElementById("dt")
+const shift = document.getElementById("shift")
 
-setInterval(() =>{
-    let date = new Date();
-    // console.log(date.toLocaleDateString());
-    clock.innerText = date.toLocaleDateString();
-    time.innerHTML = date.toLocaleTimeString();
-},1000);
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
+];
+
+
+setInterval(() => {
+
+    const date = new Date();
+
+    const dayNum = date.getDay();
+    const monthNum = date.getMonth();
+
+    day.textContent = days[dayNum]
+    hour.textContent = date.getHours() % 12;
+    minutes.textContent = date.getMinutes();
+    second.textContent = date.getSeconds();
+    dt.textContent = date.getDate() + " " + months[monthNum] + " " + date.getFullYear()
+    shift.textContent = date.getHours() > 12 ? "PM" : "AM"
+    container.style.backgroundImage = date.getHours() > 12 ? "url('https://i.pinimg.com/originals/59/62/e2/5962e280e4b5486fa60c128927bb5a06.gif')" : "url('https://i.pinimg.com/originals/1c/3e/6c/1c3e6cf5100fc66db16880281354fcdf.gif')"
+    container.style.backgroundSize = "cover";
+    container.style.backgroundPosition = "center";
+
+}, 1000);
+
